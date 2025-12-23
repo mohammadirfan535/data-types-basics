14ba1efe2f7b260391b3360d1fb24c4b8e069286
mohammadirfan535 <mohammadksirfan@gmail.com>
2025-12-23 17:30:33 +0530

    comaprison

---
+++ comparison.py
@@ -0,0 +1,17 @@
+#we
+person_1=65
+person_2=55
+com=person_1 > person_2
+print(com)
+#he
+person_3=5.9
+person_4=5.9
+com=person_3 < person_4
+print(com)
+res=person_3 > person_4
+print(res)
+#we
+person_5=(80,5.0)
+person_6=(50,5.0)
+result=person_5[0]>person_6[0]
+print(res)
