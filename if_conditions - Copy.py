3885e8b167fc5adf949520d9c49da0da55a02c2d
mohammadirfan535 <mohammadksirfan@gmail.com>
2025-12-27 16:43:39 +0530

    if else

---
+++ if_conditions - Copy.py
@@ -0,0 +1,17 @@
+market=False
+if market==True:
+	print("todays market bussiness is running fast")
+else:
+	print("today market bussiness so slow")
+
+table=True
+if table==True:
+	print("table is breaked by sir")
+else:
+	print("table is didn't break by sir")
+
+vj=False
+if vj==True:
+	print("today python class is there ")
+else:
+	print("today python class not there")
